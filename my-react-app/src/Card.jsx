a5296c51e9a912dcc76e21af549c207b0a445eb6
KraftReport <kraftreport04@gmail.com>
2024-06-11 01:05:12 +0630

    react learn

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+import ProfileImg from './assets/ippo.jpg'
+
+function Card(){
+    return(
+        <div className="card">
+            <img src={ProfileImg} alt="card image" />
+            <h2>KraftReport</h2>
+            <p>I am junior developer</p>
+        </div>
+    )
+}
+
+export default Card

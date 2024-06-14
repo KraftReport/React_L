4895ebf91ba8e19173e18c2787378c858e042e66
KraftReport <kraftreport04@gmail.com>
2024-06-15 00:58:41 +0630

    react job app

--- react-job-app/src/App.jsx
+++ react-job-app/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import HomeCard from "./Components/HomeCard";
 import JobListings from "./Components/JobListings";
+import ViewAllJobs from "./Components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -14,14 +15,7 @@ const App = () => {
 
       <JobListings />
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };

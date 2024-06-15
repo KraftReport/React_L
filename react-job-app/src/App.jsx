c6f3df496ee0306130514a45f2e21d6c4fd26542
KraftReport <kraftreport04@gmail.com>
2024-06-15 11:17:40 +0630

    react job app

--- react-job-app/src/App.jsx
+++ react-job-app/src/App.jsx
@@ -1,23 +1,21 @@
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import HomeCard from "./Components/HomeCard";
-import JobListings from "./Components/JobListings";
-import ViewAllJobs from "./Components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+ <Route path="/" element={<MainLayout/>}>
+   <Route index element={<HomePage />} />
+ </Route>
+)
+);
 const App = () => {
-  return (
-    <>
-      <Navbar />
-
-      <Hero />
-
-      <HomeCard />
-
-      <JobListings />
-
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ react-job-app/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from "../Components/Hero"
+import HomeCard from "../Components/HomeCard"
+import JobListings from "../Components/JobListings"
+import ViewAllJobs from "../Components/ViewAllJobs"
+
+const HomePage = () => {
+    return(
+        <>
+        <Hero/>
+        <HomeCard/>
+        <JobListings/>
+        <ViewAllJobs/>
+        </>
+    )
+}
+
+export default HomePage

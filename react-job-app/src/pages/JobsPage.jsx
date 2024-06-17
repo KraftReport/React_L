1ee059dd792825b18e95b2562651c5fa45b7c571
KraftReport <kraftreport04@gmail.com>
2024-06-17 23:43:15 +0630

    react job app

--- react-job-app/src/pages/JobsPage.jsx
+++ react-job-app/src/pages/JobsPage.jsx
@@ -1,4 +1,10 @@
+import JobListings from "../Components/JobListings"
+
 const JobsPage = () => {
-    return <h1>Jobs</h1>
+    return (
+        <section className="bg-blue-50 px-4 py-6">
+            <JobListings/>
+    </section>
+    )
 }
 export default JobsPage

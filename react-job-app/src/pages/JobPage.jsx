aed333011859e381ef0b4c9374d7f466024e9373
KraftReport <kraftreport04@gmail.com>
2024-06-18 16:45:40 +0630

    react job app

--- react-job-app/src/pages/JobPage.jsx
+++ react-job-app/src/pages/JobPage.jsx
@@ -1,12 +1,29 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
-import { FaMapMarker } from "react-icons/fa";
+import { FaMapMarker } from "react-icons/fa"; 
+import {toast} from "react-toastify"
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
+    const navigate = useNavigate()
   const { id } = useParams();
   const job = useLoaderData();
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this listing?'
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully');
+
+    navigate('/jobs');
+  };
+
   return (
     <>
       <section>
@@ -81,7 +98,9 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button 
+                onClick={ () =>onDeleteClick(job.id)}
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>

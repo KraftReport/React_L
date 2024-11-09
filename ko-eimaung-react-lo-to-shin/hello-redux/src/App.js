6be8ff3f9d79489206d5027e86e021cdf7bfa281
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2024-11-09 17:58:42 +0630

    day six

--- ko-eimaung-react-lo-to-shin/hello-redux/src/App.js
+++ ko-eimaung-react-lo-to-shin/hello-redux/src/App.js
@@ -2,6 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import { createRef } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router ,Routes ,Route,Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+const users  = [
+  {id : 1, name : "kyaw kyaw", gender : 'male'},
+  {id : 2, name : 'mya mya', gender : 'female'},
+  {id : 3, name : 'bo bo', gender : 'male'},
+  {id : 4,name :' po po',gender : 'female'}
+]
+
+const User = props => {
+  const {name} = useParams()
+  return (<b>{name}</b>)
+}
+
+const Male = props => {
+  return (
+    <>
+    {users.filter(i => i.gender === 'male').map(i => <li key={i.id} >{i.name}</li>)}
+    </>
+  )
+}
+
+const Female = props => {
+  return (
+    <>
+    {users.filter(i=> i.gender === 'female').map(i => <li key={i.id}>{i.name}</li>)}
+    </>
+  )
+}
 
 const Item = ({name,price}) => {
   return (
@@ -25,6 +55,32 @@ const App = props => {
 
   return (
     <>
+    <Router>
+    <ul>
+  <li><Link to="/user/alice">Alice</Link></li>
+  <li><Link to="/user/bob">Bob</Link></li>
+ </ul>
+ <div>
+ <Routes>
+    <Route path='/user/:name' element= {<User/>}></Route>
+  </Routes>
+ </div>
+    </Router>
+<Router>
+ <ul>
+  <li><Link to='/male'>Male</Link></li>
+  <li><Link to='/female'>Female</Link></li>
+ </ul>
+
+ <div>
+
+  <Routes>
+    <Route path='/male' element= {<Male/>}> </Route>
+    <Route path='/female' element = {<Female/>}></Route>
+  </Routes>
+ </div> 
+</Router>
+
     <ul>
       {props.items.map(i => 
         <Item key={i.id} name={i.name} price={i.price}/>

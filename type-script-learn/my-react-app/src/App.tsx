398363adfd68b986955affd1e69f273f391d0d01
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-01-05 00:24:48 +0630

    learning typescript

--- type-script-learn/my-react-app/src/App.tsx
+++ type-script-learn/my-react-app/src/App.tsx
@@ -1,37 +1,33 @@
-import React, {  useState } from "react"
-import './App.css'
-import InputField from './components/InputField'
-import { Todo } from "./model"
-import TodoList from "./components/TodoList"
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
-const App : React.FC = () => { 
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
 
-  const [todo,setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todos, setTodos] = useState<Todo[]>([])
-
-  
-
-  const handleAdd = (e:React.FormEvent) =>{
-    e.preventDefault()
-    if(todo){
-      setTodos([...todos,{id:Date.now(),todo,isDone:false}])
-      setTodo("")
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
     }
-  }
+  };
 
-  console.log(todo)
+  console.log(todo);
 
   return (
     <>
-    <div  className="app">
-    <h1>ToDo List</h1>
-  <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-    <TodoList todos={todos} setTodos={setTodos}/>
-    </div>
-
+      <div className="app">
+        <h1>ToDo List</h1>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- type-script-learn/my-react-app/src/components/TodoItem.tsx
+++ type-script-learn/my-react-app/src/components/TodoItem.tsx
@@ -1,35 +1,74 @@
-import { Todo } from "../model"
+import React, { useEffect, useRef, useState } from "react";
+import { Todo } from "../model";
 
-interface Props{
-    todo : Todo,
-    todos : Todo[]
-    setTodos : React.Dispatch<React.SetStateAction<Todo[]>>
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoItem : React.FC<Props> = ({todo,todos,setTodos}) =>{
+const TodoItem: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  const editRef = useRef<HTMLInputElement>(null);
 
-    const handleDone = ( id : number) => {
-        setTodos(
-            todos.map((todo)=>
-                todo.id === id ? {...todo,isDone : !todo.isDone} : todo
-            )
-        )
-    }
+  useEffect(() => {
+    editRef.current?.focus();
+  }, [edit]);
 
-    return(
-        <>
-        <div className="todoitem" key={todo.id} >
-            
-                <li >{todo.todo}</li>
-          
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-            <div>
-                <button onClick={()=>handleDone(todo.id)}>{todo.isDone ?"Undo" : "Mark Done"}</button>
-            </div>
+  const handleEdit = (id: number, e: React.FormEvent) => {
+    e.preventDefault();
+    setEdit(false);
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, todo: editTodo } : todo,
+      ),
+    );
+  };
+
+  const handleSetEdit = () => {
+    setEdit(true);
+  };
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
+  return (
+    <>
+      <div className="todoitem" key={todo.id}>
+        <li key={todo.id}>{todo.todo}</li>
+        <div>
+          {edit ? (
+            <form key={todo.id} onSubmit={(e) => handleEdit(todo.id, e)}>
+              <input
+                ref={editRef}
+                key={todo.id}
+                value={editTodo}
+                onChange={(e) => setEditTodo(e.target.value)}
+              />
+            </form>
+          ) : (
+            <button key={todo.id} onClick={() => handleSetEdit()}>
+              Edit
+            </button>
+          )}
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          <button onClick={() => handleDone(todo.id)}>
+            {todo.isDone ? "Undo" : "Mark Done"}
+          </button>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
+};
 
-}
-
-export default TodoItem
+export default TodoItem;

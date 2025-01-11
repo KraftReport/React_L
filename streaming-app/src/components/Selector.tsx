8ca7953dcaa6a53444f451140e63e442753dcab6
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-01-11 08:11:52 +0630

    streaming

---
+++ streaming-app/src/components/Selector.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+interface Props{
+    videoId : string
+}
+
+const Selector :React.FC<Props> = (videoId) => {
+
+    const [otp,setOtp] = useState<string>("")
+    const []
+
+    return(
+        <>
+        </>
+    )
+
+}
